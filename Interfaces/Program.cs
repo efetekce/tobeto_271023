4a78d544ae73b541fc01b32534a1246da4e2a3e3
efetekce <efete_kce@proton.me>
2023-10-26 16:43:48 +0300

    C# Dersleri 41: İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+interface IPerson
+{
+    int Id { get; set; }
+    int FirstName { get; set; }
+    int LastName { get; set; }
+}
+
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public int FirstName { get; set; }
+    public int LastName { get; set; }
+    public string address { get; set; }
+}
+
+class Instructor : IPerson
+{
+    public int Id { get; set; }
+    public int FirstName { get; set; }
+    public int LastName { get; set; }
+    public string department { get; set; }
+}
