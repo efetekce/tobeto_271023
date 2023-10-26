1407f6db417b61f041621d643b43a1b658284cb0
efetekce <efete_kce@proton.me>
2023-10-26 14:55:27 +0300

    C# Dersleri 39: Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -3,8 +3,20 @@ namespace Classes
 {
     public class Customer
     {
+        // when we're assigning values to properties, set method will work.
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        private string _FirstName;
+        public string FirstName
+        {
+            get
+            {
+                return "Mr " + _FirstName;
+            }
+            set
+            {
+                _FirstName = value;
+            }
+        }
         public string LastName { get; set; }
         public string City { get; set; }
     }
